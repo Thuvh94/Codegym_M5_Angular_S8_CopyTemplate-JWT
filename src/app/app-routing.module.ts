8dd8d1320a123ba3893589e463349be2dae8dc75
unknown <aloha.hat.2508@gmail.com>
2021-01-05 22:19:27 +0700

    create template and divide into components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
+import {MainComponent} from './layouts/core/main/main.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'admin',
+    component: MainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {RouterModule} from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './layouts/core/main/main.component';
+import { SidebarComponent } from './layouts/core/sidebar/sidebar.component';
+import { FooterComponent } from './layouts/core/footer/footer.component';
+import { TopbarComponent } from './layouts/core/topbar/topbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent,
+    SidebarComponent,
+    FooterComponent,
+    TopbarComponent,
+    DashboardComponent
   ],
     imports: [
         BrowserModule,
